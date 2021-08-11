7059b6013d167716cb93cb70ef9ae27311ff33a6
Kirin Sarangkasiri <k4kinny@gmail.com>
2021-08-12 04:39:34 +0700

    Updates

---
+++ precache-manifest.760729678f7aeb61416a2b459aafa9ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b061d4fb320d7065987a87afd3a114f3",
+    "url": "/cookbookgenerator/index.html"
+  },
+  {
+    "revision": "72ed98cd5523125325ac",
+    "url": "/cookbookgenerator/static/css/main.e3878e25.chunk.css"
+  },
+  {
+    "revision": "8ef6393799d2a6b44be1",
+    "url": "/cookbookgenerator/static/js/2.a9238ac5.chunk.js"
+  },
+  {
+    "revision": "f0ad72fb0c1c1e4a268423d8869404c9",
+    "url": "/cookbookgenerator/static/js/2.a9238ac5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72ed98cd5523125325ac",
+    "url": "/cookbookgenerator/static/js/main.8c2bf645.chunk.js"
+  },
+  {
+    "revision": "397f636a2f974df24ae3",
+    "url": "/cookbookgenerator/static/js/runtime-main.ec06f5a6.js"
+  },
+  {
+    "revision": "898569acdc125bcbc9d775dadbffac26",
+    "url": "/cookbookgenerator/static/media/background.898569ac.png"
+  }
+]);
